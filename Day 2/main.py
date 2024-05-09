ef9ff8a06babf089f03fe4e4ff48b9f23388194e
lhndl93 <61633800+lhndl93@users.noreply.github.com>
2024-05-09 11:54:50 +0100

    Upload Day2

---
+++ Day 2/main.py
@@ -0,0 +1,12 @@
+yourName = input("What is your name?: ")
+print("Hello " + yourName + " Now I will ask you some more questions")
+faveFood = input("What is your favourite food? ")
+print("Thankyou. Now the next one..")
+faveMusic = input("What is your favourite music?: ")
+print("Thanks, that sounds good!! Now..")
+location = input("Where do you live?: ")
+print()
+print()
+print("Thankyou for this. So your name is " + yourName +
+      ". Your favourite food is " + faveFood + ", your favourite music is " +
+      faveMusic + " and you live at " + location)
