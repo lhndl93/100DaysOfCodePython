72ca7ffe1b4a5092d3cf7996f55a88339fbd1742
lhndl93 <61633800+lhndl93@users.noreply.github.com>
2024-05-09 12:07:51 +0100

    Updated
    Created another with the variables at the top - thought this might be best practice

---
+++ Day 2/main2.py
@@ -0,0 +1,10 @@
+# created another with the variables at the top
+
+yourName = input("What is your name?: ")
+faveFood = input("What is your favourite food? ")
+faveMusic = input("What is your favourite music?: ")
+location = input("Where do you live?: ")
+
+print(f"Thankyou for this. So your name is {yourName}."
+      f"Your favourite food is {faveFood}, your favourite music is {faveMusic}"
+      f" and you live at {location}")
