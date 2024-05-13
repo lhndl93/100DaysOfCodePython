04f261038f117dd2fe6f3e4e094ed1cbe1b509d3
lhndl93 <61633800+lhndl93@users.noreply.github.com>
2024-05-13 16:41:19 +0100

    Create day 13
    Created day 13, grader. I will updated this eventually and first edge cases if the number is greater than max, or if a negative etc

---
+++ Day 13/main.py
@@ -0,0 +1,17 @@
+testName = input("What is the name of the test? ")
+maximumScore =float(input("What is the maximum score you could get? "))
+scoreReceived = float(input("What score did you get? "))
+percentage = round(scoreReceived / maximumScore * 100, 2)
+
+if percentage >= 90:
+  print(f"You got an A+ with {percentage}%")
+elif percentage >= 80:
+  print(f"You got an A  with {percentage}%")
+elif percentage >= 70:
+  print(f"You got a B with {percentage}%")
+elif percentage >= 60:
+  print(f"You got a C with {percentage}%")
+elif percentage >= 50:
+  print(f"You got a D with {percentage}%")
+else:
+  print(f"You got a U with {percentage}%")
