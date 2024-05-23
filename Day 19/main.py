3b9536cabddff6df0403987099ae60b48d4591a1
lhndl93 <61633800+lhndl93@users.noreply.github.com>
2024-05-23 15:02:05 +0100

    None

--- Day 19/main.py
+++ Day 19/main.py
@@ -1,6 +1,6 @@
 start_loan = 1000
-for counter in range (10):
+for i in range (10):
   start_loan += start_loan * 0.05
-  print(f"Year {counter + 1} is {round(start_loan, 2)}")
+  print(f"Year {i + 1} is {round(start_loan, 2)}")
   print(f"This consists of Interest: {round(start_loan * 0.05, 2)}")
   print()
