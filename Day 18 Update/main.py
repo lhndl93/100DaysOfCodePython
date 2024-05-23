d98238584bea4f532c93361a0ea54c80856e010e
lhndl93 <61633800+lhndl93@users.noreply.github.com>
2024-05-23 14:34:31 +0100

    Update main.py

--- Day 18 Update/main.py
+++ Day 18 Update/main.py
@@ -1,28 +1,28 @@
 import random
 
-myNumber = random.randint(1, 100)
+my_number = random.randint(1, 100)
 attempts = 0
-# print(myNumber)
+# print(my_number)  # Uncomment for debugging
 
-print("Gussing number game!!!")
+print("Guessing number game!!!")
 
 name = input("What is your name? ")
 
-def playerGuess():
-  guess = int(input(f"What is your guess {name}, huh?? "))
-  return guess
+def player_guess():
+    guess = int(input(f"What is your guess {name}, huh?? "))
+    return guess
 
 while True:
-  guess = playerGuess()
-  attempts += 1
-  if guess < 0:
-    print("Can't be a negative number..")
-    exit()
-  elif guess < myNumber:
-    print("oooooh too low buddy")
-  elif guess > myNumber:
-    print("Ah no, that's too high!!")
-  else:
-    print(f"Woooohooo!! you guessed the correct number {myNumber}!")
-    print(f"This took you {attempts} attempts")
-    exit()
+    guess = player_guess()
+    attempts += 1
+    if guess < 0:
+        print("Can't be a negative number..")
+        exit()
+    elif guess < my_number:
+        print("oooooh too low buddy")
+    elif guess > my_number:
+        print("Ah no, that's too high!!")
+    else:
+        print(f"Woooohooo!! you guessed the correct number {my_number}!")
+        print(f"This took you {attempts} attempts")
+        exit()
