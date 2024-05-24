4d6e76684a73a7025776098e85b04055922ee249
lhndl93 <61633800+lhndl93@users.noreply.github.com>
2024-05-24 12:23:47 +0100

    Already did this on 18, but added comments to it

---
+++ Day 22/main.py
@@ -0,0 +1,28 @@
+import random
+
+# generating a random number
+myNumber = random.randint(1, 1000000)
+attempts = 0
+# print(myNumber) # debugging
+
+print("Gussing number game!!!")
+
+while True:
+    # asking player for there guess
+    playerGuess = int(input("What is you guess, huh?? "))
+    attempts += 1
+    # checking if it is a negative number
+    if playerGuess < 0:
+        print("Can't be a negative number..")
+        exit()
+    # checking if the numbe ris too low
+    elif playerGuess < myNumber:
+        print("oooooh too low buddy")
+    # checking is number it too high
+    elif playerGuess > myNumber:
+        print("Ah no, that's too high!!")
+    # if number is correct, prints a well done
+    else:
+        print(f"Woooohooo!! you guessed the correct number {myNumber}!")
+        print(f"This took you {attempts} attempts")
+        exit()
