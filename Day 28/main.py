988d05fb92557824461c01f4f7f8fba1795ae243
lhndl93 <61633800+lhndl93@users.noreply.github.com>
2024-05-29 11:18:00 +0100

    Tried adding different types of attributes, but feel I haven't learnt enough yet to do the damage function.

--- Day 28/main.py
+++ Day 28/main.py
@@ -15,6 +15,12 @@ def character_health():
 def character_strength():
   return random_dice(12)
 
+def character_magic():
+  return random_dice(15)
+
+def character_range():
+  return random_dice(13)
+
 def health_update():
   print(f"""
   Character 1 Health: {player1_health}
@@ -39,6 +45,7 @@ def damage():
     return damage
 
 
+valid_race = ["human", "elf", "wizard", "orc"]
 while True:
     print("Welcome to the character creation.. ")
     time.sleep(1)
@@ -46,19 +53,27 @@ while True:
     time.sleep(1)
     print()
 
+    
+    
     player1_name = character_name()
     player1_race = character_race()
     player1_health = character_health()
     player1_strength = character_strength()
+    
+    # if player1_race == "wizard":
+    #   player1_magic = character_magic()
+    # elif player1_race == "elf":
+    #   player1_range = character_range()
+    # else:
+    #   player1_strength = character_strength()
 
-    if player1_race != "human" and player1_race != "elf" and player1_race != "wizard" and player1_race != "orc":
+    if player1_race not in valid_race:
         print("Incorrect race, start again please.. ")
+        os.system("clear")
         continue
-    else:
-      time.sleep(1)
-      print("Character 1 created!!") 
-      time.sleep(1)
-
+    
+    print("Character 1 created!!") 
+    time.sleep(1)
     os.system("clear")
     time.sleep(1)
 
@@ -67,16 +82,41 @@ while True:
     player2_race = character_race()
     player2_health = character_health()
     player2_strength = character_strength()
+    
+    # if player2_race == "wizard":
+    #   player2_magic = character_magic()
+    # elif player2_race == "elf":
+    #   player2_range = character_range()
+    # else:
+    #   player2_strength = character_strength()
 
-    if player2_race != "human" and player2_race != "elf" and player2_race != "wizard" and player2_race != "orc":
+    if player2_race not in valid_race:
         print("Incorrect race, start again please.. ")
+        os.system("clear")
         continue
-    else:
-        time.sleep(1)
-        print("Character 2 created!!") 
-        time.sleep(1)
+    
+    print("Character 2 created!!") 
+    time.sleep(1)
     os.system("clear")
+    time.sleep(1)
 
+    
+    # if player1_race == "wizard":
+    #   print_stats1 = f"Character Magic: {player1_magic}"
+    # elif player1_race == "elf":
+    #   print_stats1 = f"Character Range: {player1_range}"
+    # else:
+    #   print_stats1 = f"Character Strength: {player1_strength}"
+
+    # if player2_race == "wizard":
+    #   print_stats2 = f"Character Magic: {player2_magic}"
+    # elif player2_race == "elf":
+    #   print_stats2 = f"Character Range: {player2_range}"
+    # else:
+    #   print_stats2 = f"Character Strength: {player2_strength}"
+
+    print_stats1 = f"Character Strength: {player1_strength}"
+    print_stats2 = f"Character Strength: {player2_strength}"
     print(f"""
     Player stats!!
 
@@ -84,16 +124,20 @@ while True:
       Character Name: {player1_name}
       Character Race: {player1_race}
       Character Health: {player1_health}
-      Character Strength: {player1_strength}
+      {print_stats1}
 
     Player 2:
       Character Name: {player2_name}
       Character Race: {player2_race}
       Character Health: {player2_health}
-      Character Strength: {player2_strength}
+      {print_stats2}
     """)  
+    
     ready = input("Are you ready to battle?! (yes/no) ")
     if ready == "yes":
+      time.sleep(1)
+      print("Okay, lets go!!")
+      time.sleep(1)
       break
     else:
       print("Tough, we are!!")
@@ -113,6 +157,7 @@ while play_game == "yes":
 
     if player1_roll > player2_roll:
         print(f"Player 1 wins round {round} with {player1_roll}")
+        time.sleep(1)
         print(f"Damage dealt will be - {damage_int}")
         player2_health = player2_health - damage_int
         time.sleep(1)
@@ -124,24 +169,28 @@ while play_game == "yes":
 
     else:
         print(f"Player 2 wins round {round} with {player2_roll}")
+        time.sleep(1)
         print(f"Damage dealt will be - {damage_int}")
         player1_health = player1_health - damage_int
         time.sleep(1)
 
     if player1_health <= 0:
       print("Player 1 has died. Player 2 wins.")
+      time.sleep(1)
       health_update()  
       print(f"This took {round} rounds!! Good job.")
       break
       
     elif player2_health <= 0:
       print("Player 2 has died. Player 1 wins.")
+      time.sleep(1)
       health_update()  
       print(f"This took {round} rounds!! Good job.")
       break
       
     else:
       print("Both players still standing, lets go again!!")
+      time.sleep(1)
       health_update()  
       play_game = input("Are you ready for the next round? (yes/no) ")
       if play_game != "yes":
